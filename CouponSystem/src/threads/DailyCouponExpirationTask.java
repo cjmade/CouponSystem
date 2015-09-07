f13eaa94047b2fc625b5dc5a219a701668b0e593
alex <cjmade@yahoo.com>
2015-09-07 16:19:50 +0300

    No more errors

--- CouponSystem/src/threads/DailyCouponExpirationTask.java
+++ CouponSystem/src/threads/DailyCouponExpirationTask.java
@@ -1,49 +1,69 @@
 package threads;
 
 import dbAccess.CouponDBDAO;
-
-import java.sql.SQLException;
+import exceptions.ClosedConnectionStatementCreationException;
+import exceptions.ConnectionCloseException;
+import exceptions.DatabaseAccessError;
+import exceptions.WaitingForConnectionInterrupted;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.lang.Thread;
+
 import objects.Coupon;
 
 public class DailyCouponExpirationTask implements Runnable {
-
+	// Attributes
 	private long millis = 60 * 60 * 24 * 1000;
-	private CouponDBDAO coupDBDAO=new CouponDBDAO();
-
-	public DailyCouponExpirationTask() throws SQLException{
-		
+	private CouponDBDAO coupDBDAO;
+	// Constructor
+	public DailyCouponExpirationTask()
+	{
+		try	{
+			coupDBDAO = new CouponDBDAO();
+		}catch(DatabaseAccessError e)	{
+			System.out.println(e.getMessage() + ", failed to establish connection to DB");
+		}
 	}
+	// Runs in separate thread
 	@Override
 	public void run() {
-		try {
 			dailyCouponsExpirationTask();
-			System.out.println("going to sleep");
+		System.out.println("going to sleep");
+		try	{
 			Thread.sleep(millis);
-			System.out.println("wake up");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}catch(InterruptedException e)	{
+			System.out.println(e.getMessage());
 		}
-
+		System.out.println("wake up");
 	}
-
-	private void dailyCouponsExpirationTask() throws Exception {
-		ArrayList<Coupon> allCouponsFound = new ArrayList<Coupon>();
+	// Removes irrelevant coupons from the system
+	private void dailyCouponsExpirationTask() 
+	{
+		ArrayList<Coupon> allCouponsFound = null;
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		System.out.println(sdf.format(date));
-		allCouponsFound = coupDBDAO.getCouponTillDate(sdf.format(date));
+		try	{
+			allCouponsFound = coupDBDAO.getCouponTillDate(sdf.format(date));
+		}catch(WaitingForConnectionInterrupted
+				| ClosedConnectionStatementCreationException
+				| ConnectionCloseException e)	{
+			System.out.println(e.getMessage() + ", failed to access DB");
+		}
 		System.out.println(allCouponsFound.toString());
 		for (Coupon coupon : allCouponsFound) {
-			coupDBDAO.removeCoupon(coupon);
+			try	{
+				coupDBDAO.removeCoupon(coupon);
+			}catch(WaitingForConnectionInterrupted
+					| ClosedConnectionStatementCreationException
+					| ConnectionCloseException e)	{
+				System.out.println(e.getMessage() + ", failed to remove old coupon because of system exception");
+			}
 
 		}
 	}
-
+	// Stops thread
 	public void stopTask() {
 		Thread.currentThread().interrupt();
 	}
